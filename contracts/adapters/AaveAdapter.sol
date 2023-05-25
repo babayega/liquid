e1dddd2be00e06eb4f1bc486817045f5939b8fe6
shubhnkar upadhyay <shubh800@gmail.com>
2023-05-25 15:54:16 +0530

    Deployed to polygon testnet

--- contracts/adapters/AaveAdapter.sol
+++ contracts/adapters/AaveAdapter.sol
@@ -16,6 +16,11 @@ contract AaveAdapter is IAdapter {
     /// @notice POOL contract is the LP pool for the stablecoin pair
     address public immutable ASSET;
 
+    /**
+     * @notice Initializes the contract with immutable variables
+     * @param _pool is the AAVE pool
+     * @param _asset is the underlying asset contract
+     */
     constructor(address _pool, address _asset) {
         require(_pool != address(0), "!_pool");
         require(_asset != address(0), "!_asset");
@@ -24,11 +29,19 @@ contract AaveAdapter is IAdapter {
         ASSET = _asset;
     }
 
+    /**
+     * @notice Deploys the asset to the pool
+     * @param _amount is the amount of `asset` to deploy
+     */
     function deposit(uint256 _amount) external override {
         // Deposit amount into aave lending pool
         ILendingPoolV3(POOL).supply(ASSET, _amount, address(this), 0);
     }
 
+    /**
+     * @notice Withdraws the asset from the pool
+     * @param _amount is the amount of `asset` to withdraw
+     */
     function withdraw(uint256 _amount) external override {
         // Withdraw amount from the aave pool
         uint256 amountWithdrawn = ILendingPoolV3(POOL).withdraw(
@@ -39,6 +52,9 @@ contract AaveAdapter is IAdapter {
         IERC20(ASSET).transfer(msg.sender, amountWithdrawn);
     }
 
+    /**
+     * @notice Reader function to get the underlying asset
+     */
     function asset() external view override returns (address) {
         return ASSET;
     }
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,13 @@
+import * as dotenv from "dotenv";
+
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 import "hardhat-deploy";
 import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-etherscan";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -14,12 +19,25 @@ const config: HardhatUserConfig = {
       },
     },
   },
+  networks: {
+    mumbai: {
+      chainId: 80001,
+      url: process.env.MUMBAI_RPC || "",
+      accounts:
+        process.env.PVT_KEY !== undefined
+          ? [process.env.PVT_KEY]
+          : [],
+    }
+  },
   paths: {
     deploy: "scripts",
     deployments: "deployments",
   },
   namedAccounts: {
     deployer: 0
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
   },
 };
 
